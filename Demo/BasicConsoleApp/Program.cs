3be932acf7e5cec3718e966870debacf6fe60c16
Kelson Ball <kelson.ball@hey.com>
2021-06-04 16:24:03 -0700

    Add context to routing
    Add context to routing

--- Demo/BasicConsoleApp/Program.cs
+++ Demo/BasicConsoleApp/Program.cs
@@ -6,17 +6,16 @@ using static Kelson.Common.Route.Core;
 using Kelson.Common.Route.Args;
 using Kelson.Common.Parsing;
 using Kelson.Common.Route.Options;
-using System.Linq.Expressions;
 
 try
 {
-    Route(
-        On("echo" & REMAINING)
+    var c = default(ContextType<string[]>);
+    Route<string[]>(
+        a => string.Join(" ", a),
+        On(c, "echo" & REMAINING)
             | EchoCommand,
-        On("add" & Tuple(INTEGER, INTEGER))
-            | AddCommand,
-        On("add" & Options<AdditionOptions>(() => new()), Tuple(INTEGER, INTEGER))
-            | AddWithOptionsCommand)
+        On(c, "add" & Tuple(INTEGER, INTEGER))
+            | AddCommand)
         (args);
 }
 catch (Exception e)
@@ -24,44 +23,6 @@ catch (Exception e)
     Console.WriteLine(e);
 }
 
+Task EchoCommand(string[] context, string remaining) => Console.Out.WriteLineAsync(remaining);
 
-Task EchoCommand(string remaining) => Console.Out.WriteLineAsync(remaining);
-
-Task AddCommand((int a, int b) numbers) => Console.Out.WriteLineAsync($"{numbers.a} + {numbers.b} = {numbers.a + numbers.b}");
-
-async Task AddWithOptionsCommand(AdditionOptions options, (int a, int b) numbers)
-{
-    int result = options.IsSubtraction
-        ? numbers.a - numbers.b
-        : numbers.a + numbers.b;
-    ConsoleColor previousColor = Console.ForegroundColor;
-    if (options.Color is ConsoleColor assigned)
-        Console.ForegroundColor = assigned;
-    await Console.Out.WriteLineAsync($"{numbers.a} + {numbers.b} = {numbers.a + numbers.b}");
-    if (options.Color is not null)
-        Console.ForegroundColor = previousColor;
-}
-
-public class ConsoleColorArgument : TextArg<ConsoleColor>
-{
-    public override bool Matches(ref ReadOnlySpan<char> text, out ConsoleColor result)
-    {
-        result = default;
-        SpanReadingExtensions.TrimStart(ref text);
-        int delimeter = text.IndexOf(' ');
-        if (delimeter <= 0)
-            return false;
-        string name = text[..delimeter].ToString();
-        text = text[delimeter..];
-        return Enum.TryParse(name, out result);
-    }
-}
-
-record AdditionOptions(bool IsSubtraction = false, ConsoleColor? Color = null) : IOptionsModel<AdditionOptions>
-{
-    public FlagDescriptor<AdditionOptions>[] Setters => new FlagDescriptor<AdditionOptions>[]
-    {
-        Flag<AdditionOptions, bool>("subtraction", ANY, (o, sub) => o with { IsSubtraction = sub }),
-        Flag<AdditionOptions, ConsoleColor>("color", new ConsoleColorArgument(), (o, color) => o with { Color = color }),
-    };
-}
+Task AddCommand(string[] context, (int a, int b) numbers) => Console.Out.WriteLineAsync($"{numbers.a} + {numbers.b} = {numbers.a + numbers.b}");
--- Kelson.Common.Route/Command.cs
+++ Kelson.Common.Route/Command.cs
@@ -6,80 +6,80 @@ namespace Kelson.Common.Route
     using Args;
     using static RouteBuilder;
 
-    public interface IRouteCommand
+    public interface IRouteCommand<TC>
     {
         // returns a function that will return false if the route does not match the parameter,
         // or will execute the route and then return true if the route does match the parameter
-        Func<ValueTask<bool>> Query(ReadOnlySpan<char> text);
+        Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text);
     }
 
-    public class Command<T1> : IRouteCommand
+    public class Command<TC, T1> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
 
-        public Func<T1, Task> Action { get; init; }
+        public Func<TC, T1, Task> Action { get; init; }
 
-        public Command(Func<T1, Task> command) => Action = command;
+        public Command(Func<TC, T1, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
               Condition1.Matches(ref text, out T1 result1)
-            ? Yes(() => Action(result1)) : No;
+            ? Yes(() => Action(context, result1)) : No;
     }
 
-    public class Command<T1, T2> : IRouteCommand
+    public class Command<TC, T1, T2> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
 
-        public Func<T1, T2, Task> Action { get; init; }
+        public Func<TC, T1, T2, Task> Action { get; init; }
 
-        public Command(Func<T1, T2, Task> command) => Action = command;
+        public Command(Func<TC, T1, T2, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
                Condition1.Matches(ref text, out T1 result1)
             && Condition2.Matches(ref text, out T2 result2)
-            ? Yes(() => Action(result1, result2)) : No;
+            ? Yes(() => Action(context, result1, result2)) : No;
     }
 
-    public class Command<T1, T2, T3> : IRouteCommand
+    public class Command<TC, T1, T2, T3> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
         public TextArg<T3> Condition3 { get; init; }
 
-        public Func<T1, T2, T3, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, Task> Action { get; init; }
 
-        public Command(Func<T1, T2, T3, Task> command) => Action = command;
+        public Command(Func<TC, T1, T2, T3, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
                Condition1.Matches(ref text, out T1 result1)
             && Condition2.Matches(ref text, out T2 result2)
             && Condition3.Matches(ref text, out T3 result3)
-            ? Yes(() => Action(result1, result2, result3)) : No;
+            ? Yes(() => Action(context, result1, result2, result3)) : No;
     }
 
 
-    public class Command<T1, T2, T3, T4> : IRouteCommand
+    public class Command<TC, T1, T2, T3, T4> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
         public TextArg<T3> Condition3 { get; init; }
         public TextArg<T4> Condition4 { get; init; }
 
-        public Func<T1, T2, T3, T4, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, Task> Action { get; init; }
 
-        public Command(Func<T1, T2, T3, T4, Task> command) => Action = command;
+        public Command(Func<TC, T1, T2, T3, T4, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
                Condition1.Matches(ref text, out T1 result1)
             && Condition2.Matches(ref text, out T2 result2)
             && Condition3.Matches(ref text, out T3 result3)
             && Condition4.Matches(ref text, out T4 result4)
-            ? Yes(() => Action(result1, result2, result3, result4)) : No;
+            ? Yes(() => Action(context, result1, result2, result3, result4)) : No;
     }
 
 
-    public class Command<T1, T2, T3, T4, T5> : IRouteCommand
+    public class Command<TC, T1, T2, T3, T4, T5> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
@@ -87,20 +87,20 @@ namespace Kelson.Common.Route
         public TextArg<T4> Condition4 { get; init; }
         public TextArg<T5> Condition5 { get; init; }
 
-        public Func<T1, T2, T3, T4, T5, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, T5, Task> Action { get; init; }
 
-        public Command(Func<T1, T2, T3, T4, T5, Task> command) => Action = command;
+        public Command(Func<TC, T1, T2, T3, T4, T5, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
                Condition1.Matches(ref text, out T1 result1)
             && Condition2.Matches(ref text, out T2 result2)
             && Condition3.Matches(ref text, out T3 result3)
             && Condition4.Matches(ref text, out T4 result4)
             && Condition5.Matches(ref text, out T5 result5)
-            ? Yes(() => Action(result1, result2, result3, result4, result5)) : No;
+            ? Yes(() => Action(context, result1, result2, result3, result4, result5)) : No;
     }
 
-    public class Command<T1, T2, T3, T4, T5, T6> : IRouteCommand
+    public class Command<TC, T1, T2, T3, T4, T5, T6> : IRouteCommand<TC>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
@@ -109,38 +109,38 @@ namespace Kelson.Common.Route
         public TextArg<T5> Condition5 { get; init; }
         public TextArg<T6> Condition6 { get; init; }
 
-        public Func<T1, T2, T3, T4, T5, T6, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, T5, T6, Task> Action { get; init; }
 
-        public Command(Func<T1, T2, T3, T4, T5, T6, Task> command) => Action = command;
+        public Command(Func<TC, T1, T2, T3, T4, T5, T6, Task> command) => Action = command;
 
-        public Func<ValueTask<bool>> Query(ReadOnlySpan<char> text) =>
+        public Func<ValueTask<bool>> Query(TC context, ReadOnlySpan<char> text) =>
                Condition1.Matches(ref text, out T1 result1)
             && Condition2.Matches(ref text, out T2 result2)
             && Condition3.Matches(ref text, out T3 result3)
             && Condition4.Matches(ref text, out T4 result4)
             && Condition5.Matches(ref text, out T5 result5)
             && Condition6.Matches(ref text, out T6 result6)
-            ? Yes(() => Action(result1, result2, result3, result4, result5, result6)) : No;
+            ? Yes(() => Action(context, result1, result2, result3, result4, result5, result6)) : No;
     }
 
-    public class CommandCondition<T1>
+    public class CommandCondition<TC, T1>
     {
         public readonly TextArg<T1> Condition1;
 
         public CommandCondition(TextArg<T1> condition1) => Condition1 = condition1;
 
-        public Command<T1> Do(Func<T1, Task> command) => this | command;
+        public Command<TC, T1> Do(Func<TC, T1, Task> command) => this | command;
 
-        public static Command<T1> operator |(
-            CommandCondition<T1> condition,
-            Func<T1, Task> command) =>
+        public static Command<TC, T1> operator |(
+            CommandCondition<TC, T1> condition,
+            Func<TC, T1, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1
         };
     }
 
-    public class CommandCondition<T1, T2>
+    public class CommandCondition<TC, T1, T2>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
@@ -151,11 +151,11 @@ namespace Kelson.Common.Route
             (Condition1, Condition2) =
             (condition1, condition2);
 
-        public Command<T1, T2> Do(Func<T1, T2, Task> command) => this | command;
+        public Command<TC, T1, T2> Do(Func<TC, T1, T2, Task> command) => this | command;
 
-        public static Command<T1, T2> operator |(
-            CommandCondition<T1, T2> condition,
-            Func<T1, T2, Task> command) =>
+        public static Command<TC, T1, T2> operator |(
+            CommandCondition<TC, T1, T2> condition,
+            Func<TC, T1, T2, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1,
@@ -163,23 +163,23 @@ namespace Kelson.Common.Route
         };
     }
 
-    public class CommandCondition<T1, T2, T3>
+    public class CommandCondition<TC, T1, T2, T3>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
         public TextArg<T3> Condition3 { get; init; }
 
-        public Func<T1, T2, T3, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, Task> Action { get; init; }
 
         public CommandCondition(TextArg<T1> condition1, TextArg<T2> condition2, TextArg<T3> condition3) =>
             (Condition1, Condition2, Condition3) =
             (condition1, condition2, condition3);
 
-        public Command<T1, T2, T3> Do(Func<T1, T2, T3, Task> command) => this | command;
+        public Command<TC, T1, T2, T3> Do(Func<TC, T1, T2, T3, Task> command) => this | command;
 
-        public static Command<T1, T2, T3> operator |(
-            CommandCondition<T1, T2, T3> condition,
-            Func<T1, T2, T3, Task> command) =>
+        public static Command<TC, T1, T2, T3> operator |(
+            CommandCondition<TC, T1, T2, T3> condition,
+            Func<TC, T1, T2, T3, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1,
@@ -188,24 +188,24 @@ namespace Kelson.Common.Route
         };
     }
 
-    public class CommandCondition<T1, T2, T3, T4>
+    public class CommandCondition<TC, T1, T2, T3, T4>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
         public TextArg<T3> Condition3 { get; init; }
         public TextArg<T4> Condition4 { get; init; }
 
-        public Func<T1, T2, T3, T4, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, Task> Action { get; init; }
 
         public CommandCondition(TextArg<T1> condition1, TextArg<T2> condition2, TextArg<T3> condition3, TextArg<T4> condition4) =>
             (Condition1, Condition2, Condition3, Condition4) =
             (condition1, condition2, condition3, condition4);
 
-        public Command<T1, T2, T3, T4> Do(Func<T1, T2, T3, T4, Task> command) => this | command;
+        public Command<TC, T1, T2, T3, T4> Do(Func<TC, T1, T2, T3, T4, Task> command) => this | command;
 
-        public static Command<T1, T2, T3, T4> operator |(
-            CommandCondition<T1, T2, T3, T4> condition,
-            Func<T1, T2, T3, T4, Task> command) =>
+        public static Command<TC, T1, T2, T3, T4> operator |(
+            CommandCondition<TC, T1, T2, T3, T4> condition,
+            Func<TC, T1, T2, T3, T4, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1,
@@ -215,7 +215,7 @@ namespace Kelson.Common.Route
         };
     }
 
-    public class CommandCondition<T1, T2, T3, T4, T5>
+    public class CommandCondition<TC, T1, T2, T3, T4, T5>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
@@ -223,17 +223,17 @@ namespace Kelson.Common.Route
         public TextArg<T4> Condition4 { get; init; }
         public TextArg<T5> Condition5 { get; init; }
 
-        public Func<T1, T2, T3, T4, T5, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, T5, Task> Action { get; init; }
 
         public CommandCondition(TextArg<T1> condition1, TextArg<T2> condition2, TextArg<T3> condition3, TextArg<T4> condition4, TextArg<T5> condition5) =>
             (Condition1, Condition2, Condition3, Condition4, Condition5) =
             (condition1, condition2, condition3, condition4, condition5);
 
-        public Command<T1, T2, T3, T4, T5> Do(Func<T1, T2, T3, T4, T5, Task> command) => this | command;
+        public Command<TC, T1, T2, T3, T4, T5> Do(Func<TC, T1, T2, T3, T4, T5, Task> command) => this | command;
 
-        public static Command<T1, T2, T3, T4, T5> operator |(
-            CommandCondition<T1, T2, T3, T4, T5> condition,
-            Func<T1, T2, T3, T4, T5, Task> command) =>
+        public static Command<TC, T1, T2, T3, T4, T5> operator |(
+            CommandCondition<TC, T1, T2, T3, T4, T5> condition,
+            Func<TC, T1, T2, T3, T4, T5, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1,
@@ -244,7 +244,7 @@ namespace Kelson.Common.Route
         };
     }
 
-    public class CommandCondition<T1, T2, T3, T4, T5, T6>
+    public class CommandCondition<TC, T1, T2, T3, T4, T5, T6>
     {
         public TextArg<T1> Condition1 { get; init; }
         public TextArg<T2> Condition2 { get; init; }
@@ -253,17 +253,17 @@ namespace Kelson.Common.Route
         public TextArg<T5> Condition5 { get; init; }
         public TextArg<T6> Condition6 { get; init; }
 
-        public Func<T1, T2, T3, T4, T5, T6, Task> Action { get; init; }
+        public Func<TC, T1, T2, T3, T4, T5, T6, Task> Action { get; init; }
 
         public CommandCondition(TextArg<T1> condition1, TextArg<T2> condition2, TextArg<T3> condition3, TextArg<T4> condition4, TextArg<T5> condition5, TextArg<T6> condition6) =>
             (Condition1, Condition2, Condition3, Condition4, Condition5, Condition6) =
             (condition1, condition2, condition3, condition4, condition5, condition6);
 
-        public Command<T1, T2, T3, T4, T5, T6> Do(Func<T1, T2, T3, T4, T5, T6, Task> command) => this | command;
+        public Command<TC, T1, T2, T3, T4, T5, T6> Do(Func<TC, T1, T2, T3, T4, T5, T6, Task> command) => this | command;
 
-        public static Command<T1, T2, T3, T4, T5, T6> operator |(
-            CommandCondition<T1, T2, T3, T4, T5, T6> condition,
-            Func<T1, T2, T3, T4, T5, T6, Task> command) =>
+        public static Command<TC, T1, T2, T3, T4, T5, T6> operator |(
+            CommandCondition<TC, T1, T2, T3, T4, T5, T6> condition,
+            Func<TC, T1, T2, T3, T4, T5, T6, Task> command) =>
         new(command)
         {
             Condition1 = condition.Condition1,
