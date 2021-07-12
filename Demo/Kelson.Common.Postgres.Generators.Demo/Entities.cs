7bfe1ed68f42d31a3ce7f995a20cf5ffca63ff8e
Kelson Ball <kelson.ball@hey.com>
2021-07-12 13:19:19 -0700

    Add nullablity warning disable to entity default constructor creation

---
+++ Demo/Kelson.Common.Postgres.Generators.Demo/Entities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelson.Common.Postgres.Generators.Demo
+{
+    public partial record ItemCount(BigSerial Id, string Name, int Count)
+        : IPostgresEntity<ItemCount, BigSerial>;
+}
